dd25b683b7d0340a8a8b2f4367e5b9edd9647496
Demidovets Stepan <stepand555@gmail.com>
2023-12-20 10:56:05 +0300

    h

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -46,7 +46,7 @@ function Homepage(){
             {showPopup && (
                 <div className="popup">
                     <div>
-                        {<a>Это портал о самых длинных сериалах в истории кинематографа</a>}
+                        {<a>This is a portal about the longest series in the history of cinema</a>}
                     </div>
                     <button className="popup_button" onClick={handleClose}>Close</button>
                 </div>
@@ -57,12 +57,12 @@ function Homepage(){
                         <h1 className="intro_title">{title}</h1>
                         <h2 className="intro_subtitle">{time}</h2>
                     </div>
-                    <Button style={{outlineColor:'white'}} className="get_started_button" as="input" type="button" value="About"/>
+                    <Button style={{outlineColor:'white'}} className="get_started_button" as="input" type="button" value={t("About")}/>
                 </div>
             </div>
             <div className="second_slide" id="aboutID">
                 <div className="space_container2"></div>
-                <div className="space_container">Developers</div>
+                <div className="space_container">{t("Devs")}</div>
                 <div className="second_icons_container">
                     <img src="" width="80" height="80" alt="Icon1"/>
                     <img src="" width="80" height="80" alt="Icon2"/>
@@ -70,21 +70,21 @@ function Homepage(){
                 </div>
                 <div className="second_title">
                     <div className="second_title_container">
-                        <b>Krotsyuk Ivan</b>
-                        <b>Demidovets Stepan</b>
-                        <b>Symonik Vadim</b>
+                        <b>{t("Krot")}</b>
+                        <b>{t("Dem")}</b>
+                        <b>{t("Sym")}</b>
                     </div>
                 </div>
                 <div className="second_buttons">
                     <div className="second_buttons_container">
                         <a href="https://github.com/IvanKrtsk" target="_blank" rel="noreferrer">
-                            <Button className="see_more_button">View GitHub</Button>
+                            <Button className="see_more_button">{t("ViewGit")}</Button>
                         </a>
                         <a href="https://github.com/StepanDemidovets" target="_blank" rel="noreferrer">
-                            <Button className="see_more_button">View GitHub</Button>
+                            <Button className="see_more_button">{t("ViewGit")}</Button>
                         </a>
                         <a href="https://github.com/SmnVadim" target="_blank" rel="noreferrer">
-                            <Button className="see_more_button">View GitHub</Button>
+                            <Button className="see_more_button">{t("ViewGit")}</Button>
                         </a>
                     </div>
                 </div>
